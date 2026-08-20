72c0f27c363fc9ae9ee1186e9bd513ddf99171b0
gautamkumar910702 <gautamkumar910702@gmail.com>
2026-08-20 14:40:55 +0530

    Updated responsive UI

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,8 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL,
-// });
-
-// export default api;
-
 import axios from "axios";
-
-console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
-
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
-
 export default api;
